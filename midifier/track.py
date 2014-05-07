de38cab2855d340d37f93b5b960e3cd5b7645c23
Alexander Corwin <ancorwin@gmail.com>
2014-05-06 19:44:52 -0700

    Adding a channel abstraction

--- midifier/track.py
+++ midifier/track.py
@@ -3,8 +3,27 @@ from utils import _int_to_bytes
 
 
 class Track(object):
+
+  class Channel(object):
+
+    def __init__(self, channel, track):
+      self.channel = channel
+      self.track = track
+
+    def add_event(self, _class, wait_ticks, *args):
+      self.track.add_event(_class(wait_ticks, self.channel, *args))
+
+
   def __init__(self):
     self.events = []
+    self.next_channel = 0
+
+  def new_channel(self):
+    if self.next_channel > 0xF:
+      raise Exception('You can only have %d channels per track' % 0xF)
+    channel = Track.Channel(self.next_channel, self)
+    self.next_channel += 1
+    return channel
 
   def add_event(self, event):
     self.events.append(event)
